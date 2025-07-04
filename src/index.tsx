2bd4140d8c516f8eeca28249cf2539197c80a2cc
adilgashi <adilgashi0@gmail.com>
2025-07-05 01:53:07 +0200

    Fix syntax error in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-@@ .. @@
- import React from 'react';
-+import { createRoot } from 'react-dom/client';
-+import App from './App';
- 
-+const root = createRoot(document.getElementById('app')!);
-+root.render(<App />);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const root = createRoot(document.getElementById('app')!);
+root.render(<App />);
